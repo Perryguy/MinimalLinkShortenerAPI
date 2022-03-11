c7859ccd89c95d04b8d9f8b57ff061f3d2ee1cf8
Perryguy <srichardperry@gmail.com>
2022-03-11 20:03:23 +0000

    Added Frontend template

---
+++ LinkShortenerFrontend/LinkShortenerFrontend/Pages/FetchData.razor.cs
@@ -0,0 +1,15 @@
+﻿
+using LinkShortenerFrontend.Data;
+
+namespace LinkShortenerFrontend.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
